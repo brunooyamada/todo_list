6089b0e1eafa0aea2ec9186b6b2b8cc3bd65dcfa
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-11-26 17:30:11 -0300

    113. Exibindo Mensagem de Erro

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -19,6 +19,7 @@ class _TodoListPageState extends State<TodoListPage> {
 
   Todo? deletedTodo;
   int? deletedTodoPos;
+  String? errorText;
 
   @override
   void initState() {
@@ -50,6 +51,14 @@ class _TodoListPageState extends State<TodoListPage> {
                           border: OutlineInputBorder(),
                           labelText: 'Adicione uma tarefa',
                           hintText: 'Ex: Estudar Flutter',
+                          errorText: errorText,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xff00d7f3),
+                              width: 2,
+                            ),
+                          ),
+                          labelStyle: TextStyle(color: Color(0xff00d7f3)),
                         ),
                       ),
                     ),
@@ -57,12 +66,21 @@ class _TodoListPageState extends State<TodoListPage> {
                     ElevatedButton(
                       onPressed: () {
                         String text = todoController.text;
+
+                        if (text.isEmpty) {
+                          setState(() {
+                            errorText = 'O título não pode ser vazio!';
+                          });
+                          return;
+                        }
+
                         setState(() {
                           Todo newTodo = Todo(
                             title: text,
                             dateTime: DateTime.now(),
                           );
                           todos.add(newTodo);
+                          errorText = null;
                         });
                         todoController.clear();
                         todoRepository.saveTodoList(todos);
