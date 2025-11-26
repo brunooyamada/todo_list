bb47189300ba821ba5b1b1b247b4ebf3e8ef7bfb
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-11-26 15:04:54 -0300

    101. Deletando uma Tarefa

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -65,7 +65,8 @@ class _TodoListPageState extends State<TodoListPage> {
                   child: ListView(
                     shrinkWrap: true,
                     children: [
-                      for (Todo todo in todos) TodoListItem(todo: todo),
+                      for (Todo todo in todos)
+                        TodoListItem(todo: todo, onDelete: onDelete),
                     ],
                   ),
                 ),
@@ -97,5 +98,11 @@ class _TodoListPageState extends State<TodoListPage> {
         ),
       ),
     );
+  }
+
+  void onDelete(Todo todo) {
+    setState(() {
+      todos.remove(todo);
+    });
   }
 }
--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,34 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 import '../models/todo.dart';
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({super.key, required this.todo});
+  const TodoListItem({super.key, required this.todo, required this.onDelete});
 
   final Todo todo;
+  final Function(Todo) onDelete;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.grey[200],
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 2),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
-            style: TextStyle(fontSize: 12),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      child: Slidable(
+        endActionPane: ActionPane(
+          extentRatio: 0.20,
+          motion: const DrawerMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (context) {
+                onDelete(todo);
+              },
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Deletar',
+            ),
+          ],
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.grey[200],
           ),
-          Text(
-            todo.title,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+                style: TextStyle(fontSize: 12),
+              ),
+              Text(
+                todo.title,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
